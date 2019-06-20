54f24235bf96ed632e29733d2bd0ef35a00bfb21
qaz_xinlei <coolboyjrf1990@163.com>
2019-06-20 17:42:56 +0800

    222

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" ref="app" class="swiper-container">
+  <div id="app" ref="app" class="swiper-container" @click="full">
     <div class="ys-container">
       <div class="video">
        <video id="videoSource" controls="controls" ref="video" autoplay="autoplay" preload="auto" loop='loop'>
@@ -7,6 +7,8 @@
       </video>
       </div>
       <div class="right-container">
+        <div class="left-img"></div>
+        <div class="right-img"></div>
          <div class="tree xt_m1">
           <div class="xt">
             <div class="left">
@@ -222,123 +224,31 @@ export default {
   },
   mounted() {
     let _this = this
-    
     document.onkeydown=function(event) {
     var e = event || window.event || arguments.callee.caller.arguments[0];
-      // if(e && e.keyCode==9){ // 按 tab
-      //   _this.fullScreen()
-      // }
-      // if (e.keyCode === 81 && e.keyCode === 9) {
-      //   alert(1)
-      // }
       if (e.keyCode === 8) {
         _this.del = true
-        console.log(_this.del)
       }
       if (this.isTab) {
         switch (e.keyCode) {
-        case 32: 
-          _this.goOut()
-          break;
-        case 81: // Q
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m10", 0)
-          break;
-        case 87: // K
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m6", 0)
-          break;
-        case 69: // E
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m4", 0)
-          break;
-        case 82: // R
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m2", 0)
-          break;   
-        case 84: // T
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m1", 0)
-          break;   
-        case 89: // Y
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m1", 1)
-          break;   
-        case 85: // U
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m2", 1)
-          break; 
-        case 73: // I
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m4", 1)
-          break;  
-        case 79: // O
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m6", 1)
-          break; 
-        case 80: // P
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m10", 1)
-          break;
-        case 65: // A
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m9", 0)
-          break;
-        case 83: // S
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m5", 0)
-          break;
-        case 68: // D
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m3", 0)
-          break;
-        case 70: // F
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m3", 1)
-          break;
-        case 71: // G
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m5", 1)
-          break;
-        case 72: // H
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m9", 1)
-          break;
-        case 74: // J
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m8", 0)
-          break;
-        case 75: // K
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m7", 0)
-          break;
-        case 76: // L
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m8", 1)
-          break;
-        case 90: // L
-          // getStr(".xt_m7", _click, 0)
-          _this.getStr (".xt_m11", 0)
-          break;
-        case 39: // you
-          // getStr(".xt_m7", _click, 0)
-          _this.goRight()
-          break;
-        case 37: // zuo
-          // getStr(".xt_m7", _click, 0)
-          _this.goLeft()
-          break;
-        case 38: // zuo
-          // getStr(".xt_m7", _click, 0)
-          _this.source(38)
-          break;
-        case 38: // zuo
-          // getStr(".xt_m7", _click, 0)
-          _this.source(40)
-          break;
+          case 39: // you
+            // getStr(".xt_m7", _click, 0)
+            _this.goRight()
+            break;
+          case 37: // zuo
+            // getStr(".xt_m7", _click, 0)
+            _this.goLeft()
+            break;
+          case 38: // zuo
+            // getStr(".xt_m7", _click, 0)
+            _this.source(38)
+            break;
+          case 40: // zuo
+            // getStr(".xt_m7", _click, 0)
+            _this.source(40)
+            break;
         }
       }
-      console.log(e.keyCode)
       switch (e.keyCode) {
         case 32: 
           _this.goOut()
@@ -448,6 +358,13 @@ export default {
     }, 100) 
   },
   methods: {
+    // 鼠标点击全屏
+    full () {
+      let myvideo = document.querySelector('.video video')
+      myvideo.play()
+      myvideo.volume = 1
+      this.fullScreen()
+    },
     fullScreen(){
       var el = document.documentElement;
       var rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;      
@@ -515,7 +432,6 @@ export default {
             }
           })
         }
-        console.log(this.isspeak)
         return false
       } else {
         this.del = false
@@ -582,7 +498,7 @@ export default {
       if (this.isTab) {
         this.isTab = false
         // let video = $('.video video')[0]
-         let myvideo = document.querySelector('.video video')
+        let myvideo = document.querySelector('.video video')
         myvideo.play()
         myvideo.volume = 1
         $('.ys-container').animate({
@@ -621,7 +537,6 @@ export default {
   .video {
     width: 19.2rem;
     height: 10.8rem;
-    background-color: red;
     video {
       width: 100vw;
       height: 100vh;
@@ -637,6 +552,24 @@ export default {
     background: url('./../static/images/bg.png') no-repeat;
     background-size: 19.2rem 10.8rem;
     // position: absolute;
+    .left-img {
+      position: absolute;
+      left: .62rem;
+      top: .55rem;
+      width: 2.06rem;
+      height: 1.22rem;
+      background: url('./../static/images/yangsen.png');
+      background-size: 2.06rem 1.22rem;
+    }
+    .right-img {
+      position: absolute;
+      top: .48rem;
+      right: .45rem;
+      width: 2.27rem;
+      height: 1.71rem;
+      background: url('./../static/images/位图.png');
+      background-size: 2.27rem 1.71rem;
+    }
     .tree {
       display: flex;
       justify-content: space-between;
